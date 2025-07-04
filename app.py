e92ce0fc3dfd73736552a7b5651489ab5e7e3ec7
TuckerArrants <68343441+TuckerArrants@users.noreply.github.com>
2025-07-04 12:35:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def load_data_for_instrument(instrument: str, period: str = "5m") -> pd.DataFram
     """ 
     base = "https://raw.githubusercontent.com/TuckerArrants/rdr_orb/main"
     if period == "5m":
-        fname = f"{instrument}_9_30_9_30_10_25_data.csv"
+        fname = f"{instrument}_09_30_09_30_10_25_data.csv"
     else:
         raise ValueError("period must be '5m' or '15m'")
     url = f"{base}/{fname}"
